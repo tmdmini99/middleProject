251d40242e1b88a287c6539fa95d434a586aa349
miri <miri6507@naver.com>
2023-03-21 16:11:42 +0900

    수정

--- src/main/java/com/home/middle/util/Pager.java
+++ src/main/java/com/home/middle/util/Pager.java
@@ -14,6 +14,120 @@ public class Pager {
 	
 	private Long page;
 	
+	private Long startRow;
+	private Long lastRow;
+	
+	private Long startNum;
+	private Long lastNum;
+	
+	private boolean before;
+	private boolean after;
+
+	
+	public Long getProductNum() {
+		return productNum;
+	}
+
+	public void setProductNum(Long productNum) {
+		this.productNum = productNum;
+	}
+
+	public String getKind() {
+		return kind;
+	}
+
+	public void setKind(String kind) {
+		this.kind = kind;
+	}
+
+	public String getSearch() {
+		return search;
+	}
+
+	public void setSearch(String search) {
+		this.search = search;
+	}
+
+	public Long getPerPage() {
+		return perPage;
+	}
+
+	public void setPerPage(Long perPage) {
+		this.perPage = perPage;
+	}
+
+	public Long getPerBlock() {
+		return perBlock;
+	}
+
+	public void setPerBlock(Long perBlock) {
+		this.perBlock = perBlock;
+	}
+
+	public Long getTotalPage() {
+		return totalPage;
+	}
+
+	public void setTotalPage(Long totalPage) {
+		this.totalPage = totalPage;
+	}
+
+	public Long getPage() {
+		return page;
+	}
+
+	public void setPage(Long page) {
+		this.page = page;
+	}
+
+	public Long getStartRow() {
+		return startRow;
+	}
+
+	public void setStartRow(Long startRow) {
+		this.startRow = startRow;
+	}
+
+	public Long getLastRow() {
+		return lastRow;
+	}
+
+	public void setLastRow(Long lastRow) {
+		this.lastRow = lastRow;
+	}
+
+	public Long getStartNum() {
+		return startNum;
+	}
+
+	public void setStartNum(Long startNum) {
+		this.startNum = startNum;
+	}
+
+	public Long getLastNum() {
+		return lastNum;
+	}
+
+	public void setLastNum(Long lastNum) {
+		this.lastNum = lastNum;
+	}
+
+	public boolean isBefore() {
+		return before;
+	}
+
+	public void setBefore(boolean before) {
+		this.before = before;
+	}
+
+	public boolean isAfter() {
+		return after;
+	}
+
+	public void setAfter(boolean after) {
+		this.after = after;
+	}
+	
 	
 	
 }
